8866661e095987a522262dab7d0114f604dcb94a
Nguyen Lam Son <sonnl.bhsoft@gmail.com>
2022-11-01 20:46:05 +0700

    "feat: CreerSlice"

---
+++ reducers/careerSlice.ts
@@ -0,0 +1,48 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { apiInstance } from "../app/axiosClient";
+import { RootState } from "../app/store";
+import { ICareer } from "../constants/interface";
+
+export const GetAllCareerAction = createAsyncThunk('career/getAll', async () =>
+{
+  const { data } = await apiInstance.get('/career')
+  console.log("🚀 ~ file: careerSlice.ts ~ line 9 ~ data", data)
+  return data
+})
+
+interface ICareerState
+{
+  loading: 'idle' | 'loading' | 'success' | 'error'
+  careers?: ICareer[]
+}
+
+const InitialState: ICareerState = {
+  loading: 'idle',
+}
+
+const careerSlice = createSlice({
+  name: 'career',
+  initialState: InitialState,
+  reducers: {},
+  extraReducers: (builder) =>
+  {
+    builder
+      .addCase(GetAllCareerAction.pending, (state) =>
+      {
+        state.loading = 'loading'
+      })
+      .addCase(GetAllCareerAction.fulfilled, (state, action) =>
+      {
+        state.loading = 'success'
+        state.careers = action.payload
+      })
+      .addCase(GetAllCareerAction.rejected, (state, action) =>
+      {
+        state.loading = 'error'
+      })
+  }
+})
+
+export default careerSlice.reducer
+
+export const selectCareers = (state: RootState) => state.career.careers
